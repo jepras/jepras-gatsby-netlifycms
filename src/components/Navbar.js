8da10013d652c1416485d2b376a651fca86a09ac
Jeppe Rasmussen <jerasmu@microsoft.com>
2018-10-21 21:34:52 +0200

    First section

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,6 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import React from 'react';
+import { Link } from 'gatsby';
+import logo from '../img/jr.png';
 
 const Navbar = () => (
   <nav className="navbar is-transparent">
@@ -9,32 +8,41 @@ const Navbar = () => (
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
           <figure className="image">
-            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+            <img src={logo} alt="JepRas" />
           </figure>
         </Link>
       </div>
       <div className="navbar-start">
         <Link className="navbar-item" to="/about">
-          About
+          Me
+        </Link>
+        <Link className="navbar-item" to="/now">
+          Now
         </Link>
         <Link className="navbar-item" to="/products">
-          Products
+          Projects
+        </Link>
+        <Link className="navbar-item" to="/contact">
+          Contact
+        </Link>
+        <Link className="navbar-item" to="/blog">
+          Blog
         </Link>
       </div>
       <div className="navbar-end">
         <a
           className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
+          href="/"
           target="_blank"
           rel="noopener noreferrer"
         >
           <span className="icon">
-            <img src={github} alt="Github" />
+            <img src={logo} alt="Github" />
           </span>
         </a>
       </div>
     </div>
   </nav>
-)
+);
 
-export default Navbar
+export default Navbar;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,64 +1,89 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link, graphql } from 'gatsby'
-import Layout from '../components/Layout'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link, graphql } from 'gatsby';
+import Layout from '../components/Layout';
 
 export default class IndexPage extends React.Component {
   render() {
-    const { data } = this.props
-    const { edges: posts } = data.allMarkdownRemark
+    const { data } = this.props;
+    const { edges: posts } = data.allMarkdownRemark;
 
     return (
       <Layout>
+        <section className="section hero is-fullheight">
+          <div className="container has-text-centered hero-body">
+            <div className="column is-half is-offset-one-quarter">
+              <h1 className="title is-2">Hi there!</h1>
+              <h3 className="subtitle">
+                I am a technical <strong>Project Manager</strong> who is
+                constantly exploring new technologies and figuring out how they
+                can be used for a better world
+              </h3>
+            </div>
+          </div>
+          <div className="hero-foot has-text-centered">read more</div>
+        </section>
         <section className="section">
           <div className="container">
             <div className="content">
               <h1 className="has-text-weight-bold is-size-2">Latest Stories</h1>
+              <p className="subtitle">subtitle</p>
+              <p>normal</p>
+              <div class="columns">
+                <div class="column">1</div>
+                <div class="column">2</div>
+                <div class="column">3</div>
+                <div class="column">4</div>
+                <div class="column">5</div>
+              </div>
+              <a class="button is-primary" href="/">
+                Primary button
+              </a>
             </div>
-            {posts
-              .map(({ node: post }) => (
-                <div
-                  className="content"
-                  style={{ border: '1px solid #eaecee', padding: '2em 4em' }}
-                  key={post.id}
-                >
-                  <p>
-                    <Link className="has-text-primary" to={post.fields.slug}>
-                      {post.frontmatter.title}
-                    </Link>
-                    <span> &bull; </span>
-                    <small>{post.frontmatter.date}</small>
-                  </p>
-                  <p>
-                    {post.excerpt}
-                    <br />
-                    <br />
-                    <Link className="button is-small" to={post.fields.slug}>
-                      Keep Reading →
-                    </Link>
-                  </p>
-                </div>
-              ))}
+
+            {posts.map(({ node: post }) => (
+              <div
+                className="content"
+                style={{ border: '1px solid #eaecee', padding: '2em 4em' }}
+                key={post.id}
+              >
+                <p>
+                  <Link className="has-text-primary" to={post.fields.slug}>
+                    {post.frontmatter.title}
+                  </Link>
+                  <span> &bull; </span>
+                  <small>{post.frontmatter.date}</small>
+                </p>
+                <p>
+                  {post.excerpt}
+                  <br />
+                  <br />
+                  <Link className="button is-small" to={post.fields.slug}>
+                    Keep Reading →
+                  </Link>
+                </p>
+              </div>
+            ))}
           </div>
         </section>
       </Layout>
-    )
+    );
   }
 }
 
 IndexPage.propTypes = {
   data: PropTypes.shape({
     allMarkdownRemark: PropTypes.shape({
-      edges: PropTypes.array,
-    }),
-  }),
-}
+      edges: PropTypes.array
+    })
+  })
+};
 
 export const pageQuery = graphql`
   query IndexQuery {
     allMarkdownRemark(
-      sort: { order: DESC, fields: [frontmatter___date] },
-      filter: { frontmatter: { templateKey: { eq: "blog-post" } }}
+      sort: { order: DESC, fields: [frontmatter___date] }
+      filter: { frontmatter: { templateKey: { eq: "blog-post" } } }
     ) {
       edges {
         node {
@@ -76,4 +101,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
